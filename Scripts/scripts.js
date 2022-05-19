dc6470a88138cd71b2fb60a74c85746e40a08c75
AlexDavid26 <82370626+AlexDavid26@users.noreply.github.com>
2022-05-20 00:23:05 +0300

    Bootstrap

--- Scripts/scripts.js
+++ Scripts/scripts.js
@@ -1,3 +1,20 @@
+var likesNumber = new Array(8).fill(0);
+
+window.onload = function() {
+    startLikeButton();
+    var video = document.getElementById("Video");
+    video.playbackRate = 0.75;
+}
+
+function startLikeButton()
+{
+    for (var i = 0; i < 8; i++)
+    {
+        likesNumber[i] = Math.floor(Math.random() * 100);
+        document.getElementById("likesNumber" + i).innerHTML = likesNumber[i];
+    }
+}
+
 function redirect(path)
 {
     window.location.href = path
@@ -7,3 +24,26 @@ function openNewTab(path)
 {
     window.open(path, '_blank');
 }
+
+function like(value) 
+{
+    image = "image" + value;
+    var imageID = document.getElementById(image);
+
+    console.log("lol");
+
+    document.getElementById("likesNumber" + value).innerHTML = likesNumber[value];
+
+    if (imageID.src.match("/Images/heartEmpty.png"))
+    {
+        imageID.src = "/Images/heartFilled.png";
+        likesNumber[value]++;
+        document.getElementById("likesNumber" + value).innerHTML = likesNumber[value];
+    }
+    else
+    {
+        imageID.src = "/Images/heartEmpty.png";
+        likesNumber[value]--;
+        document.getElementById("likesNumber" + value).innerHTML = likesNumber[value];
+    }
+}  
